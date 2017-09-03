48d00d5979f2864313527f383b40810e323aaf8c
mike jackson <mikejackson@mikes-MacBook-Pro.local>
2017-09-03 02:32:25 -0600

    Final

---
+++ BasicCard.js
@@ -0,0 +1,10 @@
+var firstPresident = GW("Who was the first president of the United States?", "George Washington");
+
+// "Who was the first president of the United States?"
+
+if (true)
+console.log(firstPresident.front); 
+
+// "George Washington"
+console.log(firstPresident.back); 
+ 
---
+++ Js/javascript.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Memory sit amet, consectetur adipisicing elit. Atque doloremque, rem quos quis fuga repellendus maxime inventore ea nihil, soluta dolores doloribus praesentium. Tempore, eveniet, asperiores!";
+}
+
+function myFunction1() {
+    document.getElementById("demo").innerHTML = "Storage dolor sit amet, consectetur nostrum! Sed nostrum, veritatis? Incidunt sunt veniam tempora, quos voluptatibus odit quas dolorem dignissimos officia dolore similique. Nihil, omnis exercitationem.";
+}
+
+function myFunction2() {
+    document.getElementById("demo").innerHTML = "Size sit amet, consectetur adipisicing elit. Saepe molestiae magni perspiciatis, explicabo blanditiis nihil corrupti omnis cumque ea, repudiandae possimus.";
+}
+
+
+function myFunction3() {
+    document.getElementById("demo").innerHTML = "Color dolor sit amet, consectetur adip ratione, doloribus quasi inventore distinctio aperiam impedit eaque, et ab aliquid, atque dolorem dicta minus!";
+}
+
+
+
+
+
+
